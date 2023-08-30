a795cdab48da0f5f786f4fadcb1db92cdf6236b5
William Poon <willphk@outlook.com>
2023-08-30 16:35:47 -0400

    Created Task Interface and Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 import "./App.css";
+import { Task } from "./Task.ts";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
